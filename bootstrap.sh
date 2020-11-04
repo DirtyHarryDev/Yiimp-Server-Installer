07ed7d80e9cd03c62ded1f5e8816f06f91cb4d49
DirtyHarryDev <73859544+DirtyHarryDev@users.noreply.github.com>
2020-11-04 22:19:38 +0100

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+###########################################################################################################
+# Source https://mailinabox.email/ https://github.com/mail-in-a-box/mailinabox                            #
+# Updated by Dirty Harry for crypto use...                                                                #
+# This script is intended to be run like this:                                                            #
+#                                                                                                         #
+#  curl https://raw.githubusercontent.com/DirtyHarryDev/Yiimp-Server-Installer/master/bootstrap.sh | bash # 
+#                                                                                                         #
+###########################################################################################################
+
+if [ -z "${TAG}" ]; then
+	TAG=v1.0
+fi
+
+
+# Clone the Yiimp Server repository if it doesn't exist.
+if [ ! -d $HOME/yiimpserver ]; then
+	if [ ! -f /usr/bin/git ]; then
+		echo Installing git . . .
+		apt-get -q -q update
+		DEBIAN_FRONTEND=noninteractive apt-get -q -q install -y git < /dev/null
+		echo
+	fi
+
+	echo Downloading Yiimp Server Installer ${TAG}. . .
+	git clone \
+		-b ${TAG} --depth 1 \
+		https://github.com/DirtyHarryDev/yiimp_server_setup \
+		"$HOME"/yiimpserver/install \
+		< /dev/null 2> /dev/null
+
+	echo
+fi
+
+# Set permission and change directory to it.
+cd $HOME/yiimpserver/install
+
+# Update it.
+sudo chown -R $USER $HOME/yiimpserver/install/.git/
+if [ "${TAG}" != `git describe --tags` ]; then
+	echo Updating Yiimp Server Installer to ${TAG} . . .
+	git fetch --depth 1 --force --prune origin tag ${TAG}
+	if ! git checkout -q ${TAG}; then
+		echo "Update failed. Did you modify something in `pwd`?"
+		exit
+	fi
+	echo
+fi
+
+# Start setup script.
+bash $HOME/yiimpserver/install/start.sh
